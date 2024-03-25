d4030ea032305c873782c18bcbd48ddf01117f3e
YASHIRO Taketsugu <yashiro@clear-code.com>
2024-03-25 14:42:37 +0900

    Add a new main.tf for Win11-23H2 as initial commit

---
+++ verify/terraform/skus/current/windows-11-23H2/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.7.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.97.1"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~>2.5.1"
+    }
+  }
+}
+
+module "firefox_verify_env" {
+  source = "../../../modules"
+}
