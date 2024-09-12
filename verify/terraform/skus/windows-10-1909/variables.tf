4fdfe52bcac09e99356a008dab8e64fdfec90cb9
YASHIRO Taketsugu <yashiro@clear-code.com>
2024-09-12 15:53:55 +0900

    Migrate windows-10-1909

---
+++ verify/terraform/skus/windows-10-1909/variables.tf
@@ -0,0 +1,25 @@
+variable "namespace" {
+    description = "Prefix example- and the sku type to use as a part of environment name"
+    type        = string
+    default     = "example-win10-pro-1909"
+}
+variable "windows-password" {
+    description = "Windows login password, whose value is in terraform.tfvars"
+    type        = string
+}
+
+variable "offer" {
+    description = "value for storage_image_reference"
+    type        = string
+    default     = "Windows-10"
+}
+variable "publisher" {
+    description = "value for storage_image_reference"
+    type        = string
+    default     = "MicrosoftWindowsDesktop"
+}
+variable "sku" {
+    description = "value for storage_image_reference"
+    type        = string
+    default     = "19h2-pro"
+}
