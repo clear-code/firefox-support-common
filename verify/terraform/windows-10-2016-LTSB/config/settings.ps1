3981a794ba64af9ce0f425a51b8ac9d28d1b50e1
YASHIRO Taketsugu <yashiro@clear-code.com>
2024-09-13 16:28:10 +0900

    Migrate windows-10-2016-LTSB

--- verify/terraform/windows-10-2016-LTSB/config/settings.ps1
+++
@@ -1,4 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/ansible/ansible-documentation/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 -OutFile ConfigureRemotingForAnsible.ps1
-powershell -ExecutionPolicy ByPass -File ConfigureRemotingForAnsible.ps1
-Remove-Item -path ConfigureRemotingForAnsible.ps1 -force
