95ed0b31f3927174134504bdf8b3fea7ddfd65f5
YASHIRO Taketsugu <yashiro@clear-code.com>
2024-09-13 16:46:36 +0900

    Migrate windows-10-zh-cn-21H2

---
+++ verify/terraform/skus/windows-10-zh-cn-21H2/variables.tf
@@ -0,0 +1,25 @@
+variable "namespace" {
+    description = "Prefix example- and the sku type to use as a part of environment name"
+    type        = string
+    default     = "example-win10-pro-21h2-zh-cn"
+}
+variable "windows-password" {
+    description = "Windows login password, whose value is in terraform.tfvars"
+    type        = string
+}
+
+variable "offer" {
+    description = "value for storage_image_reference"
+    type        = string
+    default     = "Windows-10"
+}
+variable "publisher" {
+    description = "value for storage_image_reference"
+    type        = string
+    default     = "MicrosoftWindowsDesktop"
+}
+variable "sku" {
+    description = "value for storage_image_reference"
+    type        = string
+    default     = "win10-21h2-pro-zh-cn-g2"
+}
