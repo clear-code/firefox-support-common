f47c923fde2d7926a7ab8547c46c5490e7bf0830
YASHIRO Taketsugu <yashiro@clear-code.com>
2024-10-28 16:09:07 +0900

    Migrate windows-11-21H2

---
+++ verify/terraform/skus/windows-11-21H2/variables.tf
@@ -0,0 +1,25 @@
+variable "namespace" {
+    description = "Prefix example- and the sku type to use as a part of environment name"
+    type        = string
+    default     = "example-win11-pro-21H2"
+}
+variable "windows-password" {
+    description = "Windows login password, whose value is in terraform.tfvars"
+    type        = string
+}
+
+variable "offer" {
+    description = "value for storage_image_reference"
+    type        = string
+    default     = "Windows-11"
+}
+variable "publisher" {
+    description = "value for storage_image_reference"
+    type        = string
+    default     = "MicrosoftWindowsDesktop"
+}
+variable "sku" {
+    description = "value for storage_image_reference"
+    type        = string
+    default     = "win11-21h2-pro"
+}
