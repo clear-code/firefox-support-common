d4493f9334b361743b8d5fb33ada5b29983f98b1
YASHIRO Taketsugu <yashiro@clear-code.com>
2024-10-28 16:44:29 +0900

    Fix sku validation and windows-server-2022's value

--- verify/terraform/skus/windows-server-2022/variables.tf
+++ verify/terraform/skus/windows-server-2022/variables.tf
@@ -21,5 +21,5 @@ variable "publisher" {
 variable "sku" {
     description = "value for storage_image_reference"
     type        = string
-    default     = "2022-Datacenter"
+    default     = "2022-datacenter"
 }
